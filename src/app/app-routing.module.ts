61371018c481e25e4656c590e20766bebbbd2c6c
[sam arsanios] <[ssamseat@hotmail.com]>
2019-12-10 00:44:48 +0300

    Configured the routes to supplier components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,37 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { BuyersDashboardComponent } from './shared/buyers-dashboard/buyers-dashboard.component';
-import { PaymentnoticeComponent } from './view/buyer/paymentnotice/paymentnotice.component';
-import { AllOrdersComponent } from './view/buyer/orders/all-orders/all-orders.component';
-import { InvoicesComponent } from './view/buyer/invoices/invoices.component';
-import { OrdersComponent } from './view/buyer/orders/orders/orders.component';
-import { InviteContactsComponent } from './view/buyer/invite-contacts/invite-contacts.component';
-import { VaultComponent } from './view/buyer/vault/vault.component';
-import { PaymentInfoComponent } from './view/buyer/payment-info/payment-info.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { BuyersDashboardComponent } from "./shared/buyers-dashboard/buyers-dashboard.component";
+import { PaymentnoticeComponent } from "./view/buyer/paymentnotice/paymentnotice.component";
+import { AllOrdersComponent } from "./view/buyer/orders/all-orders/all-orders.component";
+import { InvoicesComponent } from "./view/buyer/invoices/invoices.component";
+import { OrdersComponent } from "./view/buyer/orders/orders/orders.component";
+import { InviteContactsComponent } from "./view/buyer/invite-contacts/invite-contacts.component";
+import { VaultComponent } from "./view/buyer/vault/vault.component";
+import { PaymentInfoComponent } from "./view/buyer/payment-info/payment-info.component";
+//Supplier Components
+import { SupplierPurchaseOrdersComponent } from "./view/supplier/supplier-purchase-orders/supplier-purchase-orders.component";
+import { SupplierVaultComponent } from "./view/supplier/supplier-vault/supplier-vault.component";
 
 const routes: Routes = [
- { path: 'Payment Notice', component: PaymentInfoComponent},
- { path: 'buyerdashboard', component: BuyersDashboardComponent},
- { path: 'Orders', component: OrdersComponent},
- { path: 'Invite Contacts', component:  InviteContactsComponent},
- { path: 'Invoices', component: InvoicesComponent},
- { path: 'Vault', component: VaultComponent},
-
- 
-
- 
-  
+  { path: "Payment Notice", component: PaymentInfoComponent },
+  { path: "buyerdashboard", component: BuyersDashboardComponent },
+  { path: "Orders", component: OrdersComponent },
+  { path: "Invite Contacts", component: InviteContactsComponent },
+  { path: "Invoices", component: InvoicesComponent },
+  { path: "Vault", component: VaultComponent },
+  //suppliers
+  {
+    path: "supplier-purchase-orders",
+    component: SupplierPurchaseOrdersComponent
+  },
+  {
+    path: "supplier-vault",
+    component: SupplierVaultComponent
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
