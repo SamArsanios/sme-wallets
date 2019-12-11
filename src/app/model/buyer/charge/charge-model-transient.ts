f074587c12c533e279fa28ca6927d5ffc959a586
[sam arsanios] <[ssamseat@hotmail.com]>
2019-12-11 11:45:04 +0300

    Fix the path of the charge models

--- src/app/model/buyer/charge/charge-model-transient.ts
+++ src/app/model/buyer/charge/charge-model-transient.ts
@@ -1,23 +1,45 @@
 /**
  * @author Daniel Comboni
- * 
+ *
  * a model / entity class ChargeTransient.
  */
 
-
-import { User } from '../user/user-model';
-import { Wallet } from '../wallet/wallet-model';
-import { Charge } from './charge-model';
-
+import { Charge } from "./charge-model";
+import { User } from "src/app/shared/model/user/user-model";
+import { Wallet } from "src/app/shared/model/wallet/wallet-model";
 
 export class ChargeTransient extends Charge {
+  timestampStr: string;
 
-    timestampStr: string;
-
-    constructor($id: number, $withHoldingTax: number, $discountAmount: number, $transactionFeesSupplier: number, $exciseDutySupplier: number, $transactionFeesSponsor: number, $exciseDutySponsor: number, $cbr: number, $interestRate: number, $timestamp: string, $user: User, $wallet: Wallet, $timestampStr: string) {
-        super($id, $withHoldingTax, $discountAmount, $transactionFeesSupplier, $exciseDutySupplier, $transactionFeesSponsor, $exciseDutySponsor, $cbr, $interestRate, $timestamp, $user, $wallet);
-        this.timestampStr = $timestampStr;
-    }
-
-
+  constructor(
+    $id: number,
+    $withHoldingTax: number,
+    $discountAmount: number,
+    $transactionFeesSupplier: number,
+    $exciseDutySupplier: number,
+    $transactionFeesSponsor: number,
+    $exciseDutySponsor: number,
+    $cbr: number,
+    $interestRate: number,
+    $timestamp: string,
+    $user: User,
+    $wallet: Wallet,
+    $timestampStr: string
+  ) {
+    super(
+      $id,
+      $withHoldingTax,
+      $discountAmount,
+      $transactionFeesSupplier,
+      $exciseDutySupplier,
+      $transactionFeesSponsor,
+      $exciseDutySponsor,
+      $cbr,
+      $interestRate,
+      $timestamp,
+      $user,
+      $wallet
+    );
+    this.timestampStr = $timestampStr;
+  }
 }
