c0fb1f95509adfc9e7ad1cbe062ae7f339672cdb
[sam arsanios] <[ssamseat@hotmail.com]>
2020-01-20 11:28:29 +0300

    Add login details

---
+++ src/app/service/login/login.service.ts
@@ -0,0 +1,16 @@
+// import { Injectable, OnInit } from "@angular/core";
+// import { HttpService } from "src/app/utils/http/http-service";
+// import { User } from "src/app/shared/model/user/user-model";
+
+// @Injectable({
+//   providedIn: "root"
+// })
+// export class LoginService {
+//   constructor(private httpService: HttpService<User>) {}
+
+//   tester() {
+//     this.httpService.getRequest("/users/findAll").subscribe(e => {
+//       console.log(JSON.stringify(e.body));
+//     });
+//   }
+// }
