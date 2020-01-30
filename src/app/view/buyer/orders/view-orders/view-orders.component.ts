8259d6abc504d6311b6bc85b1d89891e4326c4a2
[sam arsanios] <[ssamseat@hotmail.com]>
2020-01-30 10:30:47 +0300

    Add View Order to Orders in Buyer

---
+++ src/app/view/buyer/orders/view-orders/view-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-orders',
+  templateUrl: './view-orders.component.html',
+  styleUrls: ['./view-orders.component.css']
+})
+export class ViewOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
