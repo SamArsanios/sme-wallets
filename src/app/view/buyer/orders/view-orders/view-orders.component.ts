19e1df98c1116731e5b42532b52b875ea91d01b8
[sam arsanios] <[ssamseat@hotmail.com]>
2020-02-06 00:29:16 +0300

    Add the Pdf Generate button and logic

--- src/app/view/buyer/orders/view-orders/view-orders.component.ts
+++ src/app/view/buyer/orders/view-orders/view-orders.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-view-orders',
   templateUrl: './view-orders.component.html',
@@ -11,5 +13,10 @@ export class ViewOrdersComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  generatePdf(){
+    const documentDefinition = { content: 'This is an sample PDF printed with pdfMake' };
+    pdfMake.createPdf(documentDefinition).open();
+   }
 
 }
