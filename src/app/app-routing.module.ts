0a4506bf7c3d3c3de4b8e43135176da11e324f68
[sam arsanios] <[ssamseat@hotmail.com]>
2020-02-11 15:42:15 +0300

    Add the View Invoice component to App Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,6 +28,7 @@ import { SponsorInvoicesComponent } from "./view/sponsor/sponsor-invoices/sponso
 import { SponsorSettingsComponent } from "./view/sponsor/sponsor-settings/sponsor-settings.component";
 import { ViewOrdersComponent } from "./view/buyer/orders/view-orders/view-orders.component";
 import { SupplierViewOrdersComponent } from "./view/supplier/supplier-purchase-orders/supplier-view-orders/supplier-view-orders.component";
+import { ViewInvoicesComponent } from "./view/buyer/invoices/view-invoices/view-invoices.component";
 
 const routes: Routes = [
   //suppliers
@@ -35,6 +36,7 @@ const routes: Routes = [
   { path: "home/buyer/buyerdashboard", component: BuyersDashboardComponent },
   { path: "buyer/orders", component: OrdersComponent },
   { path: "buyer/orders/view-orders", component: ViewOrdersComponent },
+  { path: "buyer/invoices/view-invoices", component: ViewInvoicesComponent },
   { path: "buyer/Invitecontacts", component: InviteContactsComponent },
   { path: "buyer/invoices", component: InvoicesComponent },
   { path: "buyer/Vault", component: VaultComponent },
