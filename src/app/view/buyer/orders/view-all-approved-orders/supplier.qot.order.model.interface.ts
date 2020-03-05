d109aaee82c4451dc8469c51988e626e5669ecf7
[sam arsanios] <[ssamseat@hotmail.com]>
2020-03-05 09:18:52 +0300

    Build the Supplier Order data and model to fetch from supplier order

---
+++ src/app/view/buyer/orders/view-all-approved-orders/supplier.qot.order.model.interface.ts
@@ -0,0 +1,38 @@
+import {Order} from '../../../../model/buyer/order/order-model';
+import { SupplierOrder } from 'src/app/model/supplier/order/SupplierOrder';
+
+export interface ISupplierApproveOrder {
+  orderNo: any;
+  orderDate: any;
+  orderDueDate: any;
+  orderStatus: any;
+  action: any;
+}
+
+export class PopulateApproveOrderTable {
+
+  public static displayedColumns: string[] = [
+    'orderNo',
+    'orderDate',
+    'orderDueDate',
+    'orderStatus',
+    'action'
+  ];
+
+
+  public  static populateTableOnInit(fromResponse: SupplierOrder[]) {
+
+    return fromResponse.map(e => {
+
+      return  {
+        orderNo: `ORD-${e.id}`,
+        orderDate: e.timestamp,
+        orderDueDate: "not specified",
+        orderStatus: 'pending',
+        action: e.id
+      };
+
+    });
+  }
+
+}
