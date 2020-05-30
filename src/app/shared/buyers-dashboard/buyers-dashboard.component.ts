3b04c5f5edda10b8c1c88cc2f05e226e7bfcbae2
[sam arsanios] <samson.kibrom@hotmail.com>
2020-05-30 14:49:29 +0300

    Remove repeated code

--- src/app/shared/buyers-dashboard/buyers-dashboard.component.ts
+++ src/app/shared/buyers-dashboard/buyers-dashboard.component.ts
@@ -1,19 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-buyers-dashboard',
-//   templateUrl: './buyers-dashboard.component.html',
-//   styleUrls: ['./buyers-dashboard.component.css']
-// })
-// export class BuyersDashboardComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit() {
-//   }
-
-// }
-
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import {OrdersComponent} from '../../view/buyer/orders/orders/orders.component'
 
