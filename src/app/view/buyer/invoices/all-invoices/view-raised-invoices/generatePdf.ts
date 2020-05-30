5535b174c869a19eabefc53d63df2bf9039b6773
[sam arsanios] <samson.kibrom@hotmail.com>
2020-05-30 14:50:54 +0300

    Comment code

--- src/app/view/buyer/invoices/all-invoices/view-raised-invoices/generatePdf.ts
+++ src/app/view/buyer/invoices/all-invoices/view-raised-invoices/generatePdf.ts
@@ -2,6 +2,7 @@ import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
+//Code to generate invoice 
 export class RaisedInvoicePDF {
   generatePdf() {
     const documentDefinition = {
