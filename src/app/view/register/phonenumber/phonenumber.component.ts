1309f82f91af783b2f78714c744bbc816471b0ac
[sam arsanios] <samson.kibrom@hotmail.com>
2020-06-07 21:59:10 +0300

    Remove Unnecessary code

--- src/app/view/register/phonenumber/phonenumber.component.ts
+++ src/app/view/register/phonenumber/phonenumber.component.ts
@@ -7,49 +7,7 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./phonenumber.component.css']
 })
 export class PhonenumberComponent {
-//   public values = [];
-//   public form: FormGroup;
-//   phone: string;
-//   ngOnInit() {
-//     this.form = new FormGroup({
-//       phoneNumber: new FormControl({
-//         value: null,
-//         disabled: false
-//       })
-//     });
 
-//     if (this.form.controls.phoneNumber.value) {
-//       this.values = Object.keys(this.form.controls.phoneNumber.value);
-//       // console.log(this.form.controls.phoneNumber.value.dialCode + this.form.controls.phoneNumber.value.phoneNumber)
-//     }
-
-//     this.form.controls.phoneNumber.valueChanges.subscribe(value => {
-//       if (value) {
-//         this.values = Object.keys(value);
-//         // console.log(this.form.controls.phoneNumber.value.dialCode + this.form.controls.phoneNumber.value.phoneNumber)
-// // x=this.form.controls.phoneNumber.value.dialCode + this.form.controls.phoneNumber.value.phoneNumber 
-//       }
-//     });
- 
-//   }
-
-//   resetForm() {
-//     this.form.reset();
-//   }
-
-//   disable() {
-//     this.form.controls.phoneNumber.disable();
-//   }
-//   enable() {
-//     this.form.controls.phoneNumber.enable();
-//   }
-  // onchange(fom){
-  //   console.log(fom.value)
-  // }
-  // inputnumber(){
-  //   console.log(this.form.controls.phoneNumber.value.dialCode + this.form.controls.phoneNumber.value.phoneNumber)
-
-  // }
 }
 
 
