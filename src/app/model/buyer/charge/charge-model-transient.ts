e13a88900f4706bd5731da4dd22c9f58dc5a04d5
[sam arsanios] <samson.kibrom@hotmail.com>
2020-06-18 13:15:43 +0300

    Uncomment charge-model code

--- src/app/model/buyer/charge/charge-model-transient.ts
+++ src/app/model/buyer/charge/charge-model-transient.ts
@@ -1,51 +1,51 @@
-// /**
-//  * @author Daniel Comboni
-//  *
-//  * a model / entity class ChargeTransient.
-//  */
+/**
+ * @author Daniel Comboni
+ *
+ * a model / entity class ChargeTransient.
+ */
 
-// import { Charge } from "./charge-model";
-// import { User } from "src/app/shared/model/user/user-model";
-// import { Wallet } from "src/app/shared/model/wallet/wallet-model";
+import { Charge } from "./charge-model";
+import { User } from "src/app/shared/model/user/user-model";
+import { Wallet } from "src/app/shared/model/wallet/wallet-model";
 
-// export class ChargeTransient extends Charge {
-//   timestampStr: string;
+export class ChargeTransient extends Charge {
+  timestampStr: string;
 
-//   constructor(
-//     $id: number,
-//     $withHoldingTax: number,
-//     $discountAmount: number,
-//     $transactionFeesSupplier: number,
-//     $exciseDutySupplier: number,
-//     $transactionFeesSponsor: number,
-//     $exciseDutySponsor: number,
-//     $cbr: number,
-//     $interestRate: number,
-//     $timestamp: string,
-//     $user: User,
-//     $wallet: Wallet,
-//     $timestampStr: string
-//   ) {
-//     super(
-//       $id,
-//       $withHoldingTax,
-//       $discountAmount,
-//       $transactionFeesSupplier,
-//       $exciseDutySupplier,
-//       $transactionFeesSponsor,
-//       $exciseDutySponsor,
-//       $cbr,
-//       $interestRate,
-//       $timestamp,
-//       $user,
-//       $wallet
-//     );
-//     this.timestampStr = $timestampStr;
-//   }
+  constructor(
+    $id: number,
+    $withHoldingTax: number,
+    $discountAmount: number,
+    $transactionFeesSupplier: number,
+    $exciseDutySupplier: number,
+    $transactionFeesSponsor: number,
+    $exciseDutySponsor: number,
+    $cbr: number,
+    $interestRate: number,
+    $timestamp: string,
+    $user: User,
+    $wallet: Wallet,
+    $timestampStr: string
+  ) {
+    super(
+      $id,
+      $withHoldingTax,
+      $discountAmount,
+      $transactionFeesSupplier,
+      $exciseDutySupplier,
+      $transactionFeesSponsor,
+      $exciseDutySponsor,
+      $cbr,
+      $interestRate,
+      $timestamp,
+      $user,
+      $wallet
+    );
+    this.timestampStr = $timestampStr;
+  }
 
-//   static createInstance(): ChargeTransient {
-//     let newCharge = new ChargeTransient(null, null, null, null, null, null, null, null, null,
-//       null, null, null, null);
-//     return newCharge;
-//   }
-// }
+  static createInstance(): ChargeTransient {
+    let newCharge = new ChargeTransient(null, null, null, null, null, null, null, null, null,
+      null, null, null, null);
+    return newCharge;
+  }
+}
