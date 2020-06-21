53f8f02b3315558f7d2c1c70a9a99ccce0a144a8
[sam arsanios] <samson.kibrom@hotmail.com>
2020-06-21 16:03:21 +0300

    From heroku to local

--- src/app/model/buyer/invoices/invoice-model-transient.ts
+++ src/app/model/buyer/invoices/invoice-model-transient.ts
@@ -1,30 +1,30 @@
-// /**
-//  * @author Daniel Comboni
-//  * 
-//  * a model / entity class InvoiceTransient.
-//  */
+/**
+ * @author Daniel Comboni
+ * 
+ * a model / entity class InvoiceTransient.
+ */
 
-// import { User } from '../../../shared/model/user/user-model';
-// import { Wallet } from '../../../shared/model/wallet/wallet-model';
-// import { Order } from '../order/order-model';
-// import { Invoice } from './invoice-model';
+import { User } from '../../../shared/model/user/user-model';
+import { Wallet } from '../../../shared/model/wallet/wallet-model';
+import { Order } from '../order/order-model';
+import { Invoice } from './invoice-model';
 
-// export class InvoiceTransient extends Invoice {
+export class InvoiceTransient extends Invoice {
 
-//     theTimestampStr: string;
-//     invoiceDateStr: string;
-//     invoiceDueDateStr: string
+    theTimestampStr: string;
+    invoiceDateStr: string;
+    invoiceDueDateStr: string
 
-//     constructor($id: number, $order: Order, $sponsor: User, $invoiceDate: string, $invoiceDueDate: string, $invoiceStatus: string, $transactionFeePercentage: number, $transactionFees: number, $interestRate: number, $amountToPay: number, $withHoldingAmount: number, $withHoldingTaxPercentage: number, $notificationStatus: string, $buyerNotificationStatus: string, $declineReason: string, $getPaid: Boolean, $authorizeStatus: Boolean, $sponsorStatus: Boolean, $theTimestamp: string, $theTimestampStr: string, $invoiceDateStr: string) {
-//         super($id, $order, $sponsor, $invoiceDate, $invoiceDueDate, $invoiceStatus, $transactionFeePercentage, $transactionFees, $interestRate, $amountToPay, $withHoldingAmount, $withHoldingTaxPercentage, $notificationStatus, $buyerNotificationStatus, $declineReason, $getPaid, $authorizeStatus, $sponsorStatus, $theTimestamp);
-//         this.theTimestampStr = $theTimestampStr;
-//         this.invoiceDateStr = $invoiceDateStr;
-//     }
+    constructor($id: number, $order: Order, $sponsor: User, $invoiceDate: string, $invoiceDueDate: string, $invoiceStatus: string, $transactionFeePercentage: number, $transactionFees: number, $interestRate: number, $amountToPay: number, $withHoldingAmount: number, $withHoldingTaxPercentage: number, $notificationStatus: string, $buyerNotificationStatus: string, $declineReason: string, $getPaid: Boolean, $authorizeStatus: Boolean, $sponsorStatus: Boolean, $theTimestamp: string, $theTimestampStr: string, $invoiceDateStr: string) {
+        super($id, $order, $sponsor, $invoiceDate, $invoiceDueDate, $invoiceStatus, $transactionFeePercentage, $transactionFees, $interestRate, $amountToPay, $withHoldingAmount, $withHoldingTaxPercentage, $notificationStatus, $buyerNotificationStatus, $declineReason, $getPaid, $authorizeStatus, $sponsorStatus, $theTimestamp);
+        this.theTimestampStr = $theTimestampStr;
+        this.invoiceDateStr = $invoiceDateStr;
+    }
 
-//     static createInstance(): InvoiceTransient {
-//         let newInvoice = new InvoiceTransient(null, null, null, null, null, null, null, null, null,
-//             null, null, null, null, null, null, null, null, null, null, null, null);
-//         return newInvoice
-//     }
+    static createInstance(): InvoiceTransient {
+        let newInvoice = new InvoiceTransient(null, null, null, null, null, null, null, null, null,
+            null, null, null, null, null, null, null, null, null, null, null, null);
+        return newInvoice
+    }
 
-// }
+}
